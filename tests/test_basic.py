86babee1498d0c2741ccd2fa8b670e71806819a3
dhanipro <khalidalbardani209@gmail.com>
2023-03-30 14:31:03 +0700

    whatsapp otp

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,13 +6,25 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_wrong_app_id(self):
         v = VeriHubs(app_id='something', api_key='something')
-        response = v.send_sms_otp('6281310591512')
+        response = v.send_sms_otp('628131101010110')
         self.assertEqual(response.status_code, 401)
 
     def test_true_app_id(self):
         v = VeriHubs(app_id='something', api_key='something')
-        response = v.send_sms_otp('6281310591512')
+        response = v.send_sms_otp('628131101010110')
         self.assertEqual(response.status_code, 401)
+
+    
+    def test_send_whatsapp_otp(self):
+        v = VeriHubs(app_id='something', api_key='something')
+        response = v.send_whatsapp_otp('628131101010110', lang_code='id', template='otp')
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_verify_whatsapp_otp(self):
+        v = VeriHubs(app_id='something', api_key='something')
+        response = v.verify_whatsapp_otp('628131101010110', otp='558715')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
